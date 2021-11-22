0585fa1a5e47815e5ea76a3b85076534d0fa672d
Ross Patterson <me@rpatterson.net>
2021-11-22 11:55:46 -0800

    fix(formats): DRY, abstract XPath multiple matches

--- src/feedarchiver/formats.py
+++ src/feedarchiver/formats.py
@@ -7,6 +7,23 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+def query_xpath(elem, xpath):
+    """
+    Return the XPath match raising or logging errors if not one.
+    """
+    xpath_results = elem.xpath(xpath)
+    if not xpath_results:  # pragma: no cover
+        raise ValueError(f"Matched nothing for {elem.tag!r}: {xpath!r}")
+    elif len(xpath_results) != 1:  # pragma: no cover
+        logger.error(
+            "Matched %s results for %r: %r",
+            len(xpath_results),
+            elem,
+            xpath,
+        )
+    return xpath_results[0]
+
+
 class FeedFormat:
     """
     An abstract feed XML format, such as RSS or Atom.
@@ -22,17 +39,7 @@ class FeedFormat:
         """
         Return the element that contains all feed item elements per the feed format.
         """
-        items_parents = feed_root.xpath(self.ITEMS_PARENT_XPATH)
-        if not items_parents:  # pragma: no cover
-            raise ValueError(
-                f"Could not find feed items parent element: {self.ITEMS_PARENT_XPATH!r}"
-            )
-        elif len(items_parents) != 1:  # pragma: no cover
-            logger.error(
-                "Found more than one feed items parent element: %r",
-                self.ITEMS_PARENT_XPATH,
-            )
-        return items_parents[0]
+        return query_xpath(feed_root, self.ITEMS_PARENT_XPATH)
 
     def iter_items(self, feed_root):
         """
@@ -44,15 +51,7 @@ class FeedFormat:
         """
         Return the item element value that uniquely identifies it within this feed.
         """
-        item_ids = item_elem.xpath(self.ITEM_ID_XPATH)
-        if not item_ids:  # pragma: no cover
-            raise ValueError(f"Could not find feed item ID: {self.ITEM_ID_XPATH!r}")
-        elif len(item_ids) != 1:  # pragma: no cover
-            logger.error(
-                "Found more than one feed item ID: %r",
-                self.ITEM_ID_XPATH,
-            )
-        item_id = item_ids[0]
+        item_id = query_xpath(item_elem, self.ITEM_ID_XPATH)
         if not isinstance(item_id, str):  # pragma: no cover
             raise ValueError(f"Item ID is not a string: {self.ITEM_ID_XPATH!r}")
         elif not item_id.strip():  # pragma: no cover
